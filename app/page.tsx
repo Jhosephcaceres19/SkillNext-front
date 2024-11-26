10ded538f676efe4f24a5defe809c8b85b50d9d1
JHOSEPH <jhoseph@localhost.localdomain>
2024-11-26 17:29:24 -0400

    change: color and update tailwindcss v4

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='bg-linear-140/oklch from-black via-fuchsia-700 to-fuchsia-500 min-h-screen'>page</div>
   )
 }
